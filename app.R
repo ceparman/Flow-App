92b6e73a979d86918842ee362ae094c491e0591c
Craig <info@ngsanalytics.com>
2018-07-04 13:57:42 -0400

    First

---
+++ app.R
@@ -0,0 +1,31 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   flowLayout(
+        textOutput("message")
+   )
+  
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+   output$message <- renderText("Not installed yet")
+   
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
